3faebd35c06f9eaacceb5deedc79e46dcb9d0682
Jagdish Das <jagdish.das@kdyslab.com>
2024-11-21 10:20:32 +0500

    added new chagnes

--- app.py
+++ app.py
@@ -257,4 +257,4 @@ iface = gr.Interface(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
-    iface.launch(server_name="localhost", server_port=port)
+    iface.launch(server_name="0.0.0.0", server_port=port)
