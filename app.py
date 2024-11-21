f69b41b88dba4ce7762eab8d030c9cdcec5daa31
Jagdish Das <jagdish.das@kdyslab.com>
2024-11-21 14:33:55 +0500

    port changed

--- app.py
+++ app.py
@@ -282,5 +282,5 @@ app = FastAPI()
 app = gr.mount_gradio_app(app, iface, path="/")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
